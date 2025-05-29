1c0814f1248cf85b5300e28befa0ab958003f79b
Brizistance <brizistance123@gmail.com>
2025-05-29 02:42:51 -0700

    Quartz sync: May 29, 2025, 2:42 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Times New Roman",
         body: "Spectral",
         code: "IBM Plex Mono",
       },
